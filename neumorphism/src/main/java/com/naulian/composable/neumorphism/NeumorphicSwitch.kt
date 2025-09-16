d127d045827abee2acbf95682a850566f45b6061
Naulian <cinkhangin@gmail.com>
2025-09-16 21:20:48 +0800

    Sync NeumorphicSwitch active color with its thumb

--- neumorphism/src/main/java/com/naulian/composable/neumorphism/NeumorphicSwitch.kt
+++ neumorphism/src/main/java/com/naulian/composable/neumorphism/NeumorphicSwitch.kt
@@ -4,11 +4,8 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -16,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
@@ -25,9 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.shadow.Shadow
@@ -37,7 +31,6 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.naulian.modify.Gray
 import kotlinx.coroutines.delay
 
 @Composable
@@ -88,10 +81,7 @@ fun NeumorphicSwitch(
         targetValue = if(checked) MaterialTheme.colorScheme.primary
         else MaterialTheme.colorScheme.background,
         label = "accent",
-        animationSpec = tween(
-            durationMillis = 200,
-            delayMillis = 150
-        )
+        animationSpec = spring()
     )
 
     Box(
